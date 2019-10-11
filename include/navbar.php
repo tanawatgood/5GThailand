6de39b2c5e7ce0ccd51765b6567c5ab097928978
tanawatgoodboy <tanawatgood@gmail.com>
2019-10-12 01:09:47 +0700

    ลองเพิ่มการเว้นวรรคกรอบ

--- include/navbar.php
+++ include/navbar.php
@@ -17,10 +17,10 @@
                         <a class="nav-link" href="index.php">Home |<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item <?php echo $file_name == 'about' ? 'active':''  ?>">
-                        <a class="nav-link" href="about.php">Road to 5G  |</a>
+                        <a class="nav-link" href="about.php">Road to 5G&ensp;|</a>
                     </li>
                     <li class="nav-item <?php echo $file_name == 'blog'|| $file_name == 'blog-detail' ? 'active':''  ?>">
-                        <a class="nav-link" href="blog.php">Blog  |</a>
+                        <a class="nav-link" href="blog.php">Blog &ensp;|</a>
                     </li>
                     <li class="nav-item <?php echo $file_name == 'contact' ? 'active':''  ?>">
                         <a class="nav-link" href="contact.php">Contact </a>
